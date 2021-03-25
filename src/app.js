c1969011f23998db389d8c951059c214f4319690
Marcus Bertoncello <markaumvb@gmail.com>
2021-03-25 15:55:49 -0300

    primeiro commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,78 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {url, techs, title} = request.body;
+
+  const repository ={
+    id : uuid(),
+    title,
+    url, 
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const {id} = request.params;
+
+  const indice = repositories.findIndex(repository => repository.id ===id);
+  
+  if (indice === -1 ){
+    return response.status(400).json({error: "repositorio nao existe"});
+  }
+
+  const repository = {
+    id,
+    title, 
+    url, 
+    techs,
+    likes : repositories[indice].likes  
+  };
+  
+
+  repositories[indice]=repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const indice = repositories.findIndex(repository => repository.id == id);
+
+  if (indice >=0) {
+    repositories.splice(indice, 1);
+    
+  }else{
+    return response.status(400).json({erro: "repositorio não cadastrado"})
+  }
+  
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const indice = repositories.findIndex(repository => repository.id ===id);
+  
+  if (indice === -1 ){
+    return response.status(400).json({error: "repositorio nao existe"});
+  }
+
+  repositories[indice].likes ++;
+
+  return response.json(repositories[indice])
+
 });
 
 module.exports = app;
